lustre/lustre-release

commit 8e88bbfef5795b3bf9541c8cb5b2dd8a3bb11d9f

LU-12477 lustre: remove obsolete config checks
Remove from the lustre kernel code all the support for kernels
earlier than the RHEL7 3.10+. This greatly simplifies the code
and makes build times much better.

Change-Id: If52091ac5249b2719b992032040ccf30cc5bf0e4
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/37085
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_compat.h
@@ -48,31 +48,12 @@
 #include <libcfs/linux/linux-fs.h>
 #include <obd_support.h>
 
-#define current_ngroups current_cred()->group_info->ngroups
-#define current_groups current_cred()->group_info->small_block
-
 #ifdef HAVE_4ARGS_VFS_SYMLINK
 #define ll_vfs_symlink(dir, dentry, mnt, path, mode) \
                 vfs_symlink(dir, dentry, path, mode)
 #else
 #define ll_vfs_symlink(dir, dentry, mnt, path, mode) \
                        vfs_symlink(dir, dentry, path)
-#endif
-
-#if !defined(HAVE_FILE_LLSEEK_SIZE) || defined(HAVE_FILE_LLSEEK_SIZE_5ARGS)
-#define ll_generic_file_llseek_size(file, offset, origin, maxbytes, eof) \
-		generic_file_llseek_size(file, offset, origin, maxbytes, eof);
-#else
-#define ll_generic_file_llseek_size(file, offset, origin, maxbytes, eof) \
-		generic_file_llseek_size(file, offset, origin, maxbytes);
-#endif
-
-#ifdef HAVE_INODE_DIO_WAIT
-/* inode_dio_wait(i) use as-is for write lock */
-# define inode_dio_write_done(i)	do {} while (0) /* for write unlock */
-#else
-# define inode_dio_wait(i)		down_write(&(i)->i_alloc_sem)
-# define inode_dio_write_done(i)	up_write(&(i)->i_alloc_sem)
 #endif
 
 #ifndef HAVE_INIT_LIST_HEAD_RCU
@@ -91,9 +72,6 @@ static inline void INIT_LIST_HEAD_RCU(struct list_head *list)
 #define bio_idx(bio)			(bio->bi_idx)
 #define bio_set_sector(bio, sector)	(bio->bi_sector = sector)
 #define bio_sectors(bio)		((bio)->bi_size >> 9)
-#ifndef HAVE_BIO_END_SECTOR
-#define bio_end_sector(bio)		(bio->bi_sector + bio_sectors(bio))
-#endif
 #define bvl_to_page(bvl)		(bvl->bv_page)
 #endif
 
@@ -101,18 +79,6 @@ static inline void INIT_LIST_HEAD_RCU(struct list_head *list)
 #define bio_start_sector(bio) (bio->bi_iter.bi_sector)
 #else
 #define bio_start_sector(bio) (bio->bi_sector)
-#endif
-
-#ifdef HAVE_KMAP_ATOMIC_HAS_1ARG
-#define ll_kmap_atomic(a, b)	kmap_atomic(a)
-#define ll_kunmap_atomic(a, b)	kunmap_atomic(a)
-#else
-#define ll_kmap_atomic(a, b)	kmap_atomic(a, b)
-#define ll_kunmap_atomic(a, b)	kunmap_atomic(a, b)
-#endif
-
-#ifndef HAVE_CLEAR_INODE
-#define clear_inode(i)		end_writeback(i)
 #endif
 
 #ifndef HAVE_DENTRY_D_CHILD
@@ -123,29 +89,6 @@ static inline void INIT_LIST_HEAD_RCU(struct list_head *list)
 #define d_alias			d_u.d_alias
 #endif
 
-#ifndef DATA_FOR_LLITE_IS_LIST
-#define ll_d_hlist_node hlist_node
-#define ll_d_hlist_empty(list) hlist_empty(list)
-#define ll_d_hlist_entry(ptr, type, name) hlist_entry(ptr.first, type, name)
-#define ll_d_hlist_for_each(tmp, i_dentry) hlist_for_each(tmp, i_dentry)
-# ifdef HAVE_HLIST_FOR_EACH_3ARG
-# define ll_d_hlist_for_each_entry(dentry, p, i_dentry) \
-	p = NULL; hlist_for_each_entry(dentry, i_dentry, d_alias)
-# else
-# define ll_d_hlist_for_each_entry(dentry, p, i_dentry) \
-	hlist_for_each_entry(dentry, p, i_dentry, d_alias)
-# endif
-#define DECLARE_LL_D_HLIST_NODE_PTR(name) struct ll_d_hlist_node *name
-#else
-#define ll_d_hlist_node list_head
-#define ll_d_hlist_empty(list) list_empty(list)
-#define ll_d_hlist_entry(ptr, type, name) list_entry(ptr.next, type, name)
-#define ll_d_hlist_for_each(tmp, i_dentry) list_for_each(tmp, i_dentry)
-#define ll_d_hlist_for_each_entry(dentry, p, i_dentry) \
-	list_for_each_entry(dentry, i_dentry, d_alias)
-#define DECLARE_LL_D_HLIST_NODE_PTR(name) /* nothing */
-#endif /* !DATA_FOR_LLITE_IS_LIST */
-
 #ifndef HAVE_D_IN_LOOKUP
 static inline int d_in_lookup(struct dentry *dentry)
 {
@@ -153,117 +96,12 @@ static inline int d_in_lookup(struct dentry *dentry)
 }
 #endif
 
-#ifndef QUOTA_OK
-# define QUOTA_OK 0
-#endif
-#ifndef NO_QUOTA
-# define NO_QUOTA (-EDQUOT)
-#endif
-
-#ifndef SEEK_DATA
-#define SEEK_DATA      3       /* seek to the next data */
-#endif
-#ifndef SEEK_HOLE
-#define SEEK_HOLE      4       /* seek to the next hole */
-#endif
-
-#ifndef FMODE_UNSIGNED_OFFSET
-#define FMODE_UNSIGNED_OFFSET	((__force fmode_t)0x2000)
-#endif
-
-#if !defined(_ASM_GENERIC_BITOPS_EXT2_NON_ATOMIC_H_) && !defined(ext2_set_bit)
-# define ext2_set_bit             __test_and_set_bit_le
-# define ext2_clear_bit           __test_and_clear_bit_le
-# define ext2_test_bit            test_bit_le
-# define ext2_find_first_zero_bit find_first_zero_bit_le
-# define ext2_find_next_zero_bit  find_next_zero_bit_le
-#endif
-
-#ifdef ATTR_TIMES_SET
-# define TIMES_SET_FLAGS (ATTR_MTIME_SET | ATTR_ATIME_SET | ATTR_TIMES_SET)
-#else
-# define TIMES_SET_FLAGS (ATTR_MTIME_SET | ATTR_ATIME_SET)
-#endif
-
-#ifndef XATTR_NAME_POSIX_ACL_ACCESS
-# define XATTR_NAME_POSIX_ACL_ACCESS POSIX_ACL_XATTR_ACCESS
-#endif
-
-#ifndef XATTR_NAME_POSIX_ACL_DEFAULT
-# define XATTR_NAME_POSIX_ACL_DEFAULT POSIX_ACL_XATTR_DEFAULT
-#endif
-
-#ifndef HAVE_LM_XXX_LOCK_MANAGER_OPS
-# define lm_compare_owner	fl_compare_owner
-#endif
-
-/*
- * After 3.1, kernel's nameidata.intent.open.flags is different
- * with lustre's lookup_intent.it_flags, as lustre's it_flags'
- * lower bits equal to FMODE_xxx while kernel doesn't transliterate
- * lower bits of nameidata.intent.open.flags to FMODE_xxx.
- * */
-#include <linux/version.h>
-static inline int ll_namei_to_lookup_intent_flag(int flag)
-{
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 1, 0)
-	flag = (flag & ~O_ACCMODE) | OPEN_FMODE(flag);
-#endif
-	return flag;
-}
-
-#include <linux/fs.h>
-#ifndef HAVE_PROTECT_I_NLINK
-static inline void set_nlink(struct inode *inode, unsigned int nlink)
-{
-	inode->i_nlink = nlink;
-}
-#endif
-
-#ifdef HAVE_INODEOPS_USE_UMODE_T
-# define ll_umode_t	umode_t
-#else
-# define ll_umode_t	int
-#endif
-
 #ifndef HAVE_VM_FAULT_T
 #define vm_fault_t int
 #endif
 
-#include <linux/dcache.h>
-#ifndef HAVE_D_MAKE_ROOT
-static inline struct dentry *d_make_root(struct inode *root)
-{
-	struct dentry *res = d_alloc_root(root);
-
-	if (res == NULL && root)
-		iput(root);
-
-	return res;
-}
-#endif
-
 #ifndef HAVE_FOP_ITERATE_SHARED
 #define iterate_shared iterate
-#endif
-
-#ifdef HAVE_DIRTY_INODE_HAS_FLAG
-# define ll_dirty_inode(inode, flag)	(inode)->i_sb->s_op->dirty_inode((inode), flag)
-#else
-# define ll_dirty_inode(inode, flag)	(inode)->i_sb->s_op->dirty_inode((inode))
-#endif
-
-#ifdef HAVE_FILE_F_INODE
-# define set_file_inode(file, inode)	(file)->f_inode = inode
-#else
-# define set_file_inode(file, inode)
-#endif
-
-#ifndef HAVE_FILE_INODE
-static inline struct inode *file_inode(const struct file *file)
-{
-	return file->f_path.dentry->d_inode;
-}
 #endif
 
 #ifdef HAVE_OLDSIZE_TRUNCATE_PAGECACHE
@@ -286,20 +124,14 @@ static inline struct inode *file_inode(const struct file *file)
 #define ll_vfs_unlink(a, b) vfs_unlink(a, b)
 #endif
 
-#ifndef HAVE_INODE_OWNER_OR_CAPABLE
-#define inode_owner_or_capable(inode) is_owner_or_cap(inode)
-#endif
-
 static inline int ll_vfs_getattr(struct path *path, struct kstat *st)
 {
 	int rc;
 
 #ifdef HAVE_INODEOPS_ENHANCED_GETATTR
 	rc = vfs_getattr(path, st, STATX_BASIC_STATS, AT_STATX_SYNC_AS_STAT);
-#elif defined HAVE_VFS_GETATTR_2ARGS
-	rc = vfs_getattr(path, st);
 #else
-	rc = vfs_getattr(path->mnt, path->dentry, st);
+	rc = vfs_getattr(path, st);
 #endif
 	return rc;
 }
@@ -311,23 +143,10 @@ static inline bool d_is_positive(const struct dentry *dentry)
 }
 #endif
 
-#ifdef HAVE_VFS_CREATE_USE_NAMEIDATA
-# define LL_VFS_CREATE_FALSE NULL
-#else
-# define LL_VFS_CREATE_FALSE false
-#endif
-
 #ifndef HAVE_INODE_LOCK
 # define inode_lock(inode) mutex_lock(&(inode)->i_mutex)
 # define inode_unlock(inode) mutex_unlock(&(inode)->i_mutex)
 # define inode_trylock(inode) mutex_trylock(&(inode)->i_mutex)
-#endif
-
-#ifndef HAVE_RADIX_EXCEPTION_ENTRY
-static inline int radix_tree_exceptional_entry(void *arg)
-{
-	return 0;
-}
 #endif
 
 #ifndef HAVE_XA_IS_VALUE
@@ -352,35 +171,6 @@ static inline int __must_check PTR_ERR_OR_ZERO(__force const void *ptr)
 	else
 		return 0;
 }
-#endif
-
-#ifndef SIZE_MAX
-#define SIZE_MAX	(~(size_t)0)
-#endif
-
-#ifdef HAVE_SECURITY_IINITSEC_CALLBACK
-# define ll_security_inode_init_security(inode, dir, name, value, len, \
-					 initxattrs, dentry)	       \
-	 security_inode_init_security(inode, dir, &((dentry)->d_name), \
-				      initxattrs, dentry)
-#elif defined HAVE_SECURITY_IINITSEC_QSTR
-# define ll_security_inode_init_security(inode, dir, name, value, len, \
-					 initxattrs, dentry)	       \
-	 security_inode_init_security(inode, dir, &((dentry)->d_name), \
-				      name, value, len)
-#else /* !HAVE_SECURITY_IINITSEC_CALLBACK && !HAVE_SECURITY_IINITSEC_QSTR */
-# define ll_security_inode_init_security(inode, dir, name, value, len, \
-					 initxattrs, dentry)	       \
-	 security_inode_init_security(inode, dir, name, value, len)
-#endif
-
-#ifndef bio_for_each_segment_all /* since kernel version 3.9 */
-#ifdef HAVE_BVEC_ITER
-#define bio_for_each_segment_all(bv, bio, it) \
-	for (it = 0, bv = (bio)->bi_io_vec; it < (bio)->bi_vcnt; it++, bv++)
-#else
-#define bio_for_each_segment_all(bv, bio, it) bio_for_each_segment(bv, bio, it)
-#endif
 #endif
 
 #ifdef HAVE_PID_NS_FOR_CHILDREN
@@ -436,16 +226,6 @@ __vfs_setxattr(struct dentry *dentry, struct inode *inode, const char *name,
 }
 #endif /* HAVE_VFS_SETXATTR */
 
-#ifndef HAVE_POSIXACL_USER_NS
-/*
- * Mask out &init_user_ns so we don't jump
- * through hoops to define it somehow only
- * to have it ignored anyway.
- */
-#define posix_acl_from_xattr(a, b, c)	posix_acl_from_xattr(b, c)
-#define posix_acl_to_xattr(a, b, c, d)	posix_acl_to_xattr(b, c, d)
-#endif
-
 #ifndef HAVE_POSIX_ACL_VALID_USER_NS
 #define posix_acl_valid(a,b)		posix_acl_valid(b)
 #endif
@@ -480,17 +260,6 @@ static inline void iov_iter_truncate(struct iov_iter *i, u64 count)
 	if (i->count > count)
 		i->count = count;
 }
-#endif
-
-#ifndef HAVE_IS_SXID
-static inline bool is_sxid(umode_t mode)
-{
-	return (mode & S_ISUID) || ((mode & S_ISGID) && (mode & S_IXGRP));
-}
-#endif
-
-#ifndef IS_NOSEC
-#define IS_NOSEC(inode)	(!is_sxid(inode->i_mode))
 #endif
 
 /*
@@ -634,39 +403,8 @@ static inline struct timespec current_time(struct inode *inode)
 }
 #endif
 
-#ifndef time_after32
-/**
- * time_after32 - compare two 32-bit relative times
- * @a: the time which may be after @b
- * @b: the time which may be before @a
- *
- * time_after32(a, b) returns true if the time @a is after time @b.
- * time_before32(b, a) returns true if the time @b is before time @a.
- *
- * Similar to time_after(), compare two 32-bit timestamps for relative
- * times.  This is useful for comparing 32-bit seconds values that can't
- * be converted to 64-bit values (e.g. due to disk format or wire protocol
- * issues) when it is known that the times are less than 68 years apart.
- */
-#define time_after32(a, b)     ((s32)((u32)(b) - (u32)(a)) < 0)
-#define time_before32(b, a)    time_after32(a, b)
-
-#endif
-
-#ifndef __GFP_COLD
-#define __GFP_COLD 0
-#endif
-
-#ifndef alloc_workqueue
-#define alloc_workqueue(name, flags, max_active) create_workqueue(name)
-#endif
-
 #ifndef smp_store_mb
 #define smp_store_mb(var, value)	set_mb(var, value)
-#endif
-
-#ifndef READ_ONCE
-#define READ_ONCE ACCESS_ONCE
 #endif
 
 #if IS_ENABLED(CONFIG_BLK_DEV_INTEGRITY)

libcfs/include/libcfs/linux/linux-list.h
@@ -25,26 +25,6 @@
 
 #include <linux/list.h>
 
-#ifdef HAVE_HLIST_FOR_EACH_3ARG
-#define cfs_hlist_for_each_entry(tpos, pos, head, member) \
-	hlist_for_each_entry(tpos, head, member)
-#define cfs_hlist_for_each_entry_safe(tpos, pos, n, head, member) \
-	hlist_for_each_entry_safe(tpos, n, head, member)
-#define cfs_hlist_for_each_entry_continue(tpos, pos, member) \
-	hlist_for_each_entry_continue(tpos, member)
-#define cfs_hlist_for_each_entry_from(tpos, pos, member) \
-	hlist_for_each_entry_from(tpos, member)
-#else
-#define cfs_hlist_for_each_entry(tpos, pos, head, member) \
-	hlist_for_each_entry(tpos, pos, head, member)
-#define cfs_hlist_for_each_entry_safe(tpos, pos, n, head, member) \
-	hlist_for_each_entry_safe(tpos, pos, n, head, member)
-#define cfs_hlist_for_each_entry_continue(tpos, pos, member) \
-	hlist_for_each_entry_continue(tpos, pos, member)
-#define cfs_hlist_for_each_entry_from(tpos, pos, member) \
-	hlist_for_each_entry_from(tpos, pos, member)
-#endif
-
 #ifdef HAVE_HLIST_ADD_AFTER
 #define hlist_add_behind(hnode, tail)	hlist_add_after(tail, hnode)
 #endif /* HAVE_HLIST_ADD_AFTER */

lustre/include/lprocfs_status.h
@@ -538,29 +538,6 @@ lprocfs_register(const char *name, struct proc_dir_entry *parent,
 extern void lprocfs_remove(struct proc_dir_entry **root);
 extern void lprocfs_remove_proc_entry(const char *name,
                                       struct proc_dir_entry *parent);
-#ifndef HAVE_REMOVE_PROC_SUBTREE
-extern int remove_proc_subtree(const char *name,
-			       struct proc_dir_entry *parent);
-#define PDE_DATA(inode)		(PDE(inode)->data)
-
-static inline int LPROCFS_ENTRY_CHECK(struct inode *inode)
-{
-	struct proc_dir_entry *dp = PDE(inode);
-	int deleted = 0;
-
-	spin_lock(&(dp)->pde_unload_lock);
-	if (dp->proc_fops == NULL)
-		deleted = 1;
-	spin_unlock(&(dp)->pde_unload_lock);
-	if (deleted)
-		return -ENODEV;
-	return 0;
-}
-#else
-static inline int LPROCFS_ENTRY_CHECK(struct inode *inode)
-{ return 0; }
-#endif
-
 extern int lprocfs_obd_setup(struct obd_device *obd, bool uuid_only);
 extern int lprocfs_obd_cleanup(struct obd_device *obd);
 #ifdef HAVE_SERVER_SUPPORT
@@ -748,12 +725,6 @@ static const struct file_operations name##_fops = {			\
 #define __LPROC_SEQ_FOPS(name, custom_seq_write)			\
 static int name##_single_open(struct inode *inode, struct file *file)	\
 {									\
-	int rc;								\
-									\
-	rc = LPROCFS_ENTRY_CHECK(inode);				\
-	if (rc < 0)							\
-		return rc;						\
-									\
 	return single_open(file, name##_seq_show,			\
 			   inode->i_private ? inode->i_private :	\
 					      PDE_DATA(inode));		\

lustre/llite/dcache.c
@@ -224,7 +224,6 @@ void ll_intent_release(struct lookup_intent *it)
 void ll_invalidate_aliases(struct inode *inode)
 {
 	struct dentry *dentry;
-	DECLARE_LL_D_HLIST_NODE_PTR(p);
 	ENTRY;
 
 	LASSERT(inode != NULL);
@@ -233,7 +232,7 @@ void ll_invalidate_aliases(struct inode *inode)
 	       PFID(ll_inode2fid(inode)), inode);
 
 	spin_lock(&inode->i_lock);
-	ll_d_hlist_for_each_entry(dentry, p, &inode->i_dentry) {
+	hlist_for_each_entry(dentry, &inode->i_dentry, d_alias) {
 		CDEBUG(D_DENTRY, "dentry in drop %.*s (%p) parent %p "
 		       "inode %p flags %d\n", dentry->d_name.len,
 		       dentry->d_name.name, dentry, dentry->d_parent,
@@ -292,6 +291,9 @@ static int ll_revalidate_dentry(struct dentry *dentry,
 {
 	struct inode *dir = dentry->d_parent->d_inode;
 
+	CDEBUG(D_VFSTRACE, "VFS Op:name=%s, flags=%u\n",
+	       dentry->d_name.name, lookup_flags);
+
 	/* If this is intermediate component path lookup and we were able to get
 	 * to this dentry, then its lock has not been revoked and the
 	 * path component is valid. */
@@ -324,44 +326,8 @@ static int ll_revalidate_dentry(struct dentry *dentry,
 	return 1;
 }
 
-/*
- * Always trust cached dentries. Update statahead window if necessary.
- */
-#ifdef HAVE_IOP_ATOMIC_OPEN
-static int ll_revalidate_nd(struct dentry *dentry, unsigned int flags)
-{
-	int rc;
-	ENTRY;
-
-	CDEBUG(D_VFSTRACE, "VFS Op:name=%s, flags=%u\n",
-	       dentry->d_name.name, flags);
-
-	rc = ll_revalidate_dentry(dentry, flags);
-	RETURN(rc);
-}
-#else
-static int ll_revalidate_nd(struct dentry *dentry, struct nameidata *nd)
-{
-	int rc;
-	ENTRY;
-
-	/*
-	 * this is normally called from NFS export, and we don't know whether
-	 * this is the last component.
-	 */
-	if (nd == NULL)
-		RETURN(1);
-
-	CDEBUG(D_VFSTRACE, "VFS Op:name=%s, flags=%u\n",
-	       dentry->d_name.name, nd->flags);
-
-	rc = ll_revalidate_dentry(dentry, nd->flags);
-	RETURN(rc);
-}
-#endif
-
 const struct dentry_operations ll_d_ops = {
-        .d_revalidate = ll_revalidate_nd,
+	.d_revalidate	= ll_revalidate_dentry,
         .d_release = ll_release,
         .d_delete  = ll_ddelete,
         .d_compare = ll_dcompare,

lustre/llite/file.c
@@ -422,14 +422,14 @@ static inline int ll_dom_readpage(void *data, struct page *page)
 	struct niobuf_local *lnb = data;
 	void *kaddr;
 
-	kaddr = ll_kmap_atomic(page, KM_USER0);
+	kaddr = kmap_atomic(page);
 	memcpy(kaddr, lnb->lnb_data, lnb->lnb_len);
 	if (lnb->lnb_len < PAGE_SIZE)
 		memset(kaddr + lnb->lnb_len, 0,
 		       PAGE_SIZE - lnb->lnb_len);
 	flush_dcache_page(page);
 	SetPageUptodate(page);
-	ll_kunmap_atomic(kaddr, KM_USER0);
+	kunmap_atomic(kaddr);
 	unlock_page(page);
 
 	return 0;
@@ -3981,73 +3981,6 @@ out_state:
 	}
 }
 
-#ifndef HAVE_FILE_LLSEEK_SIZE
-static inline loff_t
-llseek_execute(struct file *file, loff_t offset, loff_t maxsize)
-{
-	if (offset < 0 && !(file->f_mode & FMODE_UNSIGNED_OFFSET))
-		return -EINVAL;
-	if (offset > maxsize)
-		return -EINVAL;
-
-	if (offset != file->f_pos) {
-		file->f_pos = offset;
-		file->f_version = 0;
-	}
-	return offset;
-}
-
-static loff_t
-generic_file_llseek_size(struct file *file, loff_t offset, int origin,
-                loff_t maxsize, loff_t eof)
-{
-	struct inode *inode = file_inode(file);
-
-	switch (origin) {
-	case SEEK_END:
-		offset += eof;
-		break;
-	case SEEK_CUR:
-		/*
-		 * Here we special-case the lseek(fd, 0, SEEK_CUR)
-		 * position-querying operation.  Avoid rewriting the "same"
-		 * f_pos value back to the file because a concurrent read(),
-		 * write() or lseek() might have altered it
-		 */
-		if (offset == 0)
-			return file->f_pos;
-		/*
-		 * f_lock protects against read/modify/write race with other
-		 * SEEK_CURs. Note that parallel writes and reads behave
-		 * like SEEK_SET.
-		 */
-		inode_lock(inode);
-		offset = llseek_execute(file, file->f_pos + offset, maxsize);
-		inode_unlock(inode);
-		return offset;
-	case SEEK_DATA:
-		/*
-		 * In the generic case the entire file is data, so as long as
-		 * offset isn't at the end of the file then the offset is data.
-		 */
-		if (offset >= eof)
-			return -ENXIO;
-		break;
-	case SEEK_HOLE:
-		/*
-		 * There is a virtual hole at the end of the file, so as long as
-		 * offset isn't i_size or larger, return i_size.
-		 */
-		if (offset >= eof)
-			return -ENXIO;
-		offset = eof;
-		break;
-	}
-
-	return llseek_execute(file, offset, maxsize);
-}
-#endif
-
 static loff_t ll_file_seek(struct file *file, loff_t offset, int origin)
 {
 	struct inode *inode = file_inode(file);
@@ -4068,8 +4001,8 @@ static loff_t ll_file_seek(struct file *file, loff_t offset, int origin)
 		eof = i_size_read(inode);
 	}
 
-	retval = ll_generic_file_llseek_size(file, offset, origin,
-					     ll_file_maxbytes(inode), eof);
+	retval = generic_file_llseek_size(file, offset, origin,
+					  ll_file_maxbytes(inode), eof);
 	if (retval >= 0)
 		ll_stats_ops_tally(ll_i2sbi(inode), LPROC_LL_LLSEEK,
 				   ktime_us_delta(ktime_get(), kstart));
@@ -5104,9 +5037,7 @@ struct inode_operations ll_file_inode_operations = {
 #endif
 	.listxattr	= ll_listxattr,
 	.fiemap		= ll_fiemap,
-#ifdef HAVE_IOP_GET_ACL
 	.get_acl	= ll_get_acl,
-#endif
 #ifdef HAVE_IOP_SET_ACL
 	.set_acl	= ll_set_acl,
 #endif

lustre/llite/llite_internal.h
@@ -73,6 +73,8 @@
 
 #define LL_IT2STR(it) ((it) ? ldlm_it2str((it)->it_op) : "0")
 
+#define TIMES_SET_FLAGS (ATTR_MTIME_SET | ATTR_ATIME_SET | ATTR_TIMES_SET)
+
 struct ll_dentry_data {
 	struct lookup_intent		*lld_it;
 	unsigned int			lld_sa_generation;
@@ -403,19 +405,19 @@ int ll_xattr_cache_get(struct inode *inode,
 
 static inline bool obd_connect_has_secctx(struct obd_connect_data *data)
 {
-#if defined(HAVE_SECURITY_DENTRY_INIT_SECURITY) && defined(CONFIG_SECURITY)
+#ifdef CONFIG_SECURITY
 	return data->ocd_connect_flags & OBD_CONNECT_FLAGS2 &&
 	       data->ocd_connect_flags2 & OBD_CONNECT2_FILE_SECCTX;
 #else
 	return false;
-#endif /* HAVE_SECURITY_DENTRY_INIT_SECURITY */
+#endif
 }
 
 static inline void obd_connect_set_secctx(struct obd_connect_data *data)
 {
-#if defined(HAVE_SECURITY_DENTRY_INIT_SECURITY) && defined(CONFIG_SECURITY)
+#ifdef CONFIG_SECURITY
 	data->ocd_connect_flags2 |= OBD_CONNECT2_FILE_SECCTX;
-#endif /* HAVE_SECURITY_DENTRY_INIT_SECURITY */
+#endif
 }
 
 int ll_dentry_init_security(struct dentry *dentry, int mode, struct qstr *name,
@@ -1092,11 +1094,7 @@ int ll_iocontrol(struct inode *inode, struct file *file,
 int ll_flush_ctx(struct inode *inode);
 void ll_umount_begin(struct super_block *sb);
 int ll_remount_fs(struct super_block *sb, int *flags, char *data);
-#ifdef HAVE_SUPEROPS_USE_DENTRY
 int ll_show_options(struct seq_file *seq, struct dentry *dentry);
-#else
-int ll_show_options(struct seq_file *seq, struct vfsmount *vfs);
-#endif
 void ll_dirty_page_discard_warn(struct page *page, int ioret);
 int ll_prep_inode(struct inode **inode, struct ptlrpc_request *req,
 		  struct super_block *, struct lookup_intent *);

lustre/llite/llite_lib.c
@@ -1775,11 +1775,8 @@ int ll_setattr_raw(struct dentry *dentry, struct iattr *attr,
 		       (s64)attr->ia_mtime.tv_sec, (s64)attr->ia_ctime.tv_sec,
 		       ktime_get_real_seconds());
 
-	if (S_ISREG(inode->i_mode)) {
-		if (attr->ia_valid & ATTR_SIZE)
-			inode_dio_write_done(inode);
+	if (S_ISREG(inode->i_mode))
 		inode_unlock(inode);
-	}
 
 	/* We always do an MDS RPC, even if we're only changing the size;
 	 * only the MDS knows whether truncate() should fail with -ETXTBUSY */
@@ -2759,21 +2756,12 @@ void ll_finish_md_op_data(struct md_op_data *op_data)
         OBD_FREE_PTR(op_data);
 }
 
-#ifdef HAVE_SUPEROPS_USE_DENTRY
 int ll_show_options(struct seq_file *seq, struct dentry *dentry)
-#else
-int ll_show_options(struct seq_file *seq, struct vfsmount *vfs)
-#endif
 {
 	struct ll_sb_info *sbi;
 
-#ifdef HAVE_SUPEROPS_USE_DENTRY
-	LASSERT((seq != NULL) && (dentry != NULL));
+	LASSERT(seq && dentry);
 	sbi = ll_s2sbi(dentry->d_sb);
-#else
-	LASSERT((seq != NULL) && (vfs != NULL));
-	sbi = ll_s2sbi(vfs->mnt_sb);
-#endif
 
 	if (sbi->ll_flags & LL_SBI_NOLCK)
 		seq_puts(seq, ",nolock");

neilbrown/lustre

commit fc5c839ba0fef2067ec8746237fe821163b87eb0

LU-12477 lustre: restore time_after32()
time_after32 is needed when compiling with kernels earlier than v1.14,
which haven't had it backported.  E.g. SLE12-SP3-LTSS needs it.

Fixes: 8e88bbfef579 ("LU-12477 lustre: remove obsolete config checks")
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: Ifb51b9ed0945ad28f8e8aa34dada3860107b95df

lustre/include/lustre_compat.h
@@ -403,6 +403,27 @@ static inline struct timespec current_time(struct inode *inode)
 }
 #endif
 
+#ifndef time_after32
+/**
+ * time_after32 - compare two 32-bit relative times
+ * @a: the time which may be after @b
+ * @b: the time which may be before @a
+ *
+ * Needed for kernels earlier than v4.14-rc1~134^2
+ *
+ * time_after32(a, b) returns true if the time @a is after time @b.
+ * time_before32(b, a) returns true if the time @b is before time @a.
+ *
+ * Similar to time_after(), compare two 32-bit timestamps for relative
+ * times.  This is useful for comparing 32-bit seconds values that can't
+ * be converted to 64-bit values (e.g. due to disk format or wire protocol
+ * issues) when it is known that the times are less than 68 years apart.
+ */
+#define time_after32(a, b)     ((s32)((u32)(b) - (u32)(a)) < 0)
+#define time_before32(b, a)    time_after32(a, b)
+
+#endif
+
 #ifndef smp_store_mb
 #define smp_store_mb(var, value)	set_mb(var, value)
 #endif