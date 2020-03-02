lustre/lustre-release

commit 3453c95f513cd9d65f8e74e64d10d8942d934f01

LU-13258 libcfs: make apply_workqueue_attrs() available for Lustre
Currently Lustre work queues can run on any core which introduces
noise on the system. The linux kernel has a function called
apply_workqueue_attrs() which allows you to control which cores
a work queue will execute on. Manually export this function so
Lustre can use it.

Test-Parameters: trivial

Change-Id: I467539cb8def7029fa9dfff2386234de5e0fe133
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/37613
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/linux/linux-prim.c
@@ -31,6 +31,8 @@
  */
 
 #define DEBUG_SUBSYSTEM S_LNET
+
+#include <linux/kallsyms.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
@@ -101,6 +103,27 @@ time64_t ktime_get_seconds(void)
 }
 EXPORT_SYMBOL(ktime_get_seconds);
 #endif /* HAVE_KTIME_GET_SECONDS */
+
+static int (*cfs_apply_workqueue_attrs_t)(struct workqueue_struct *wq,
+					  const struct workqueue_attrs *attrs);
+
+int cfs_apply_workqueue_attrs(struct workqueue_struct *wq,
+			      const struct workqueue_attrs *attrs)
+{
+	return cfs_apply_workqueue_attrs_t(wq, attrs);
+}
+EXPORT_SYMBOL_GPL(cfs_apply_workqueue_attrs);
+
+struct kmem_cache (*cfs_radix_tree_node_cachep);
+
+void cfs_arch_init(void)
+{
+#ifndef HAVE_WAIT_VAR_EVENT
+	wait_bit_init();
+#endif
+	cfs_apply_workqueue_attrs_t =
+		(void *)kallsyms_lookup_name("apply_workqueue_attrs");
+}
 
 int cfs_kernel_write(struct file *filp, const void *buf, size_t count,
 		     loff_t *pos)

libcfs/include/libcfs/linux/linux-misc.h
@@ -85,10 +85,16 @@ static inline void kernel_param_lock(struct module *mod)
 }
 #endif /* ! HAVE_KERNEL_PARAM_LOCK */
 
+int cfs_apply_workqueue_attrs(struct workqueue_struct *wq,
+			      const struct workqueue_attrs *attrs);
+
 #ifndef HAVE_KSTRTOBOOL_FROM_USER
 
 #define kstrtobool strtobool
 
 int kstrtobool_from_user(const char __user *s, size_t count, bool *res);
-#endif
-#endif
+#endif /* HAVE_KSTRTOBOOL_FROM_USER */
+
+void cfs_arch_init(void);
+
+#endif /* __LIBCFS_LINUX_MISC_H__ */

libcfs/libcfs/module.c
@@ -719,9 +719,8 @@ static int __init libcfs_init(void)
 {
 	int rc;
 
-#ifndef HAVE_WAIT_VAR_EVENT
-	wait_bit_init();
-#endif
+	cfs_arch_init();
+
 	rc = libcfs_debug_init(5 * 1024 * 1024);
 	if (rc < 0) {
 		pr_err("LustreError: libcfs_debug_init: rc = %d\n", rc);

neilbrown/lustre

commit 1437c961ef05e277030af6d3d5493df0ca49062f

LU-13258 libcfs: fixes for cfs_arch_init()
The introduction of cfs_arch_init() brought two problems.

1/ wait_bit_init() wasn't known due to a missing include file.
2/ cfs_arch_init() was not marked __init, but it called
   a function (wait_bit_init) that was.

This patch fixes both of these.

Fixes: 3453c95f513c ("LU-13258 libcfs: make apply_workqueue_attrs() available for Lustre")
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: I6f19e11e68f52ca8071332364d369ed3a717d5c9

libcfs/libcfs/linux/linux-prim.c
@@ -49,6 +49,7 @@
 #endif
 
 #include <libcfs/linux/linux-time.h>
+#include <libcfs/linux/linux-wait.h>
 
 #ifndef HAVE_KTIME_GET_TS64
 void ktime_get_ts64(struct timespec64 *ts)
@@ -116,7 +117,7 @@ EXPORT_SYMBOL_GPL(cfs_apply_workqueue_attrs);
 
 struct kmem_cache (*cfs_radix_tree_node_cachep);
 
-void cfs_arch_init(void)
+void __init cfs_arch_init(void)
 {
 #ifndef HAVE_WAIT_VAR_EVENT
 	wait_bit_init();