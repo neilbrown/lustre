4a05420875217f28675e18d5e8d259e9e48e033e
Mr NeilBrown <neilb@suse.de>
2020-08-07 12:05:35 +1000

    LU-9855 lustre: use with_imp_locked() more broadly.
    Several places in lustre take u.cli.cl_sem to protect access to
    u.cli.cl_import, and so could use with_imp_locked() achieving cleaner
    code.
    
    Using with_imp_locked() in functions calling
    ptlrpc_set_import_active() requires care as that function gets a
    write-lock on ->cl_sem.  So they need to use with_imp_locked() only to
    get a counted reference on the imp, and must drop the lock before
    calling ptlrpc_set_import_active().
    
    This patch makes those changes and also:
    
    - introduces with_imp_locked_nested() for sptlrpc_conf_client_adapt(),
    - re-indents obd_cleanup_client_import(), which is only tangentially
      related the the main purpose of this patch,
    - removes code in ldlm_flock_completion_ast() which takes a copy
      of cl_import, and doesn't use it.
    - adds with_imp_locked() to two functions named 'active_store' which
      weren't using it but should
    - removes with_imp_locked() from ping_show() and instead includes it
      in ptlrpc_obd_ping() where 'imp' is actually used.
    
    Signed-off-by: Mr NeilBrown <neilb@suse.de>
    Change-Id: I01a713c200a1698af222bc72cf4f955227a98305

--- lustre/include/lprocfs_status.h
+++ lustre/include/lprocfs_status.h
@@ -648,12 +648,16 @@ extern int lprocfs_seq_release(struct inode *, struct file *);
 /* You must use these macros when you want to refer to
  * the import in a client obd_device for a lprocfs entry
  */
-#define with_imp_locked(__obd, __imp, __rc)				\
-	for (down_read(&(__obd)->u.cli.cl_sem),				\
+
+#define with_imp_locked_nested(__obd, __imp, __rc, __nest)		\
+	for (down_read_nested(&(__obd)->u.cli.cl_sem, __nest),		\
 	     __imp = (__obd)->u.cli.cl_import,				\
 	     __rc = __imp ? 0 : -ENODEV;				\
 	     __imp ? 1 : (up_read(&(__obd)->u.cli.cl_sem), 0);		\
 	     __imp = NULL)
+
+#define with_imp_locked(__obd, __imp, __rc)	\
+	with_imp_locked_nested(__obd, __imp, __rc, 0)
 
 /* write the name##_seq_show function, call LDEBUGFS_SEQ_FOPS_RO for read-only
  * debugfs entries; otherwise, you will define name##_seq_write function also
